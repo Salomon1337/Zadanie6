ca89a6e5002ce7bcf7200b823ca297ed6bc627fb
Salomon1337 <salomonpl2000@gmail.com>
2021-11-29 19:55:35 +0100

    Fix bugs

--- app/src/main/java/com/example/zadanie6/MainActivity.java
+++ app/src/main/java/com/example/zadanie6/MainActivity.java
@@ -6,20 +6,24 @@ import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.UUID;
+
+public class MainActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        UUID taskId = (UUID) getIntent().getSerializableExtra(TaskListFragment.KEY_EXTRA_TASK_ID);
+        return TaskFragment.newInstance(taskId);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
 
-        if(fragment == null){
-            fragment = new TaskFragment();
-            fragmentManager.beginTransaction()
-                    .add(R.id.fragment_container,fragment)
-                    .commit();
-        }
     }
+
+
+
 }
--- app/src/main/java/com/example/zadanie6/TaskFragment.java
+++ app/src/main/java/com/example/zadanie6/TaskFragment.java
@@ -10,47 +10,70 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import java.util.UUID;
 
 public class TaskFragment extends Fragment {
-    private Task task;
     private EditText nameField;
+    private Task task;
     private Button dateButton;
     private CheckBox doneCheckBox;
-
+    private static final String ARG_TASK_ID = "task_id";
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState){
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        this.task = new Task();
         super.onCreate(savedInstanceState);
-        task = new Task();
+        UUID taskId = (UUID) getArguments().getSerializable(ARG_TASK_ID);
+        task = TaskStorage.getInstance().getTask(taskId);
+
     }
 
     @Nullable
     @Override
-    public View onCreateView(@Nullable LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_task, container, false);
         nameField = view.findViewById(R.id.task_name);
-        nameField.addTextChangedListener (new TextWatcher(){
+        nameField.setText(task.getName());
+        nameField.addTextChangedListener(new TextWatcher() {
+
             @Override
-            public void beforeTextChanged (CharSequence s, int start, int count, int after){
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
+
             @Override
-            public void  onTextChanged (CharSequence s, int start, int before, int count){
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
                 task.setName(s.toString());
             }
+
             @Override
-            public void afterTextChanged (Editable s){
+            public void afterTextChanged(Editable s) {
 
             }
         });
+
+        dateButton = view.findViewById(R.id.task_date);
         dateButton.setText(task.getDate().toString());
         dateButton.setEnabled(false);
 
+        doneCheckBox = view.findViewById(R.id.task_done);
         doneCheckBox.setChecked(task.isDone());
-        doneCheckBox.setOnCheckedChangeListener(((buttonView, isChecked) -> task.setDone(isChecked)));
+        doneCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            task.setDone(true);
+        });
+
         return view;
     }
 
-
+    public static TaskFragment newInstance(UUID taskId) {
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(ARG_TASK_ID, taskId);
+        TaskFragment taskFragment = new TaskFragment();
+        taskFragment.setArguments(bundle);
+        return taskFragment;
+    }
 }
